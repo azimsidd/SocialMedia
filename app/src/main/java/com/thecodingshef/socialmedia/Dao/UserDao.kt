fba8d7aea2666f602822f1a42f4b177f4ba62f3b
azim567 <kaziazeemsiddiqui786@gmail.com>
2021-01-03 23:25:07 +0530

    second commit

--- app/src/main/java/com/thecodingshef/socialmedia/Dao/UserDao.kt
+++ app/src/main/java/com/thecodingshef/socialmedia/Dao/UserDao.kt
@@ -2,6 +2,8 @@ package com.thecodingshef.socialmedia.Dao
 
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.thecodingshef.socialmedia.Model.User
@@ -23,8 +25,10 @@ class UserDao {
             }
 
         }
+    }
 
+    fun getUserById(uId:String):Task<DocumentSnapshot>{
 
-
+        return userCollections.document(uId).get()
     }
 }
--- app/src/main/java/com/thecodingshef/socialmedia/MainActivity.kt
+++ app/src/main/java/com/thecodingshef/socialmedia/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.thecodingshef.socialmedia
 
+import android.app.DownloadManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.Query
+import com.thecodingshef.socialmedia.Dao.PostDao
+import com.thecodingshef.socialmedia.Model.Post
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+   private lateinit var postAdapter:PostAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +22,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        floatingActionButton.setOnClickListener {
 
+            val intent=Intent(this,CreatePostActivity::class.java)
+            startActivity(intent)
+        }
+
+        setupRecyclerview()
+
+    }
+
+    private fun setupRecyclerview() {
+
+        val postDao=PostDao()
+        val query=postDao.postCollection.orderBy("createdBy",Query.Direction.DESCENDING)
+
+        val recyclerOptions=FirestoreRecyclerOptions.Builder<Post>().setQuery(query,Post::class.java).build()
+        postAdapter= PostAdapter(recyclerOptions)
+
+        recyclerView.adapter=postAdapter
+        recyclerView.layoutManager=LinearLayoutManager(this)
+
+        recyclerView.setHasFixedSize(true)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        postAdapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        postAdapter.stopListening()
     }
 }
